76a856aebc5b86f10fa533d5c63a6cefb846eea0
Umutoni123-aa <u.nada@alustudent.com>
2025-11-11 13:53:44 +0000

    updating the api_advanced on the task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""
+Script that fetches the titles of the first 10 hot posts
+for a given subreddit.
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
+    """Prints the titles of the first 10 hot posts for a valid subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'MyRedditApp/1.0'}
+    params = {'limit': 10}
 
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
+    if response.status_code != 200:
         print(None)
+        return
+
+    data = response.json().get("data", {}).get("children", [])
+
+    if not data:
+        print(None)
+        return
+
+    for post in data:
+        print(post.get("data", {}).get("title"))
